9b73aed066b3cc6ee3f84122a76c9ed1c93c24bf
antonio <antonioiksi@gmail.com>
2018-05-22 23:35:14 +0300

    Initial Commit

---
+++ IrrigationClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IrrigationClient
+//
+//  Created by Antonchikov Alexander on 22/05/2018.
+//  Copyright © 2018 Antonchikov Alexander. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
